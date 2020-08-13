03cade21866e5c980c1dfb9bfc6fe5052f0c4990
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2020-08-13 11:03:30 -0400

    Find the largest element in a vector

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-fn black_box_function(x: i32, y: i32) -> i32 {
-    x + y
+fn black_box_function(x: Vec<i32>) -> Vec<i32>{
+    println!("Made it to black box");
+    let mut z= vec![];
+    for i in &x{
+        z.push(i+2);        
+    }
+    z
 }
 
 fn main() {
     println!("Hello, world!");
-    let x = 5;
-    let y = 6;
-    let z = black_box_function(x, y);
-    println!("The value of z is {}", z);
+    let x = vec![1, 2, 3];
+
+    let z=black_box_function(x);
+    let mut largest =0;
+    let mut largest_index=0;
+    let mut index=0;
+    for i in &z{
+        if(i>&largest){
+            largest=*i;
+            largest_index=index;
+        }
+        println!("The value of z in {}", i);
+        index= index+1;
+    }
+    println!("The largest value is {}", largest);
+    println!("The index of it is {}", largest_index);
+    
 }
